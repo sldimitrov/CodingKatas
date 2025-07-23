73c2215787839577f4c34bc67bd91aaa7b87d084
Slavi FE <slavidimitrov54@gmail.com>
2025-07-23 15:12:59 +0300

    kata(2) Implement yet another solution to the kata problem

---
+++ kata2/index.js
@@ -0,0 +1,60 @@
+// Binary Search Algorithm Implementation #1
+// const targetValue = 10;
+// const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//
+// let leftIdx = 0;
+// let rightIdx = sortedArray.length;
+//
+// for (let i = 0; i <= sortedArray.length; i++) {
+//   let midIndex = Math.floor((leftIdx + rightIdx) / 2);
+//
+//   const currentValue = sortedArray[midIndex];
+//
+//   if (currentValue === targetValue) {
+//     break;
+//   } else if (currentValue < targetValue) {
+//     leftIdx = midIndex;
+//   } else if (currentValue > targetValue) {
+//     rightIdx = midIndex;
+//   }
+//
+//   if (midIndex === 0 || midIndex === sortedArray.length - 1) {
+//     break;
+//   }
+// }
+
+// Binary Search Algorithm Implementation #2
+const target = 9;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let left = 0;
+let right = arr.length - 1;
+let found = false;
+
+while (left <= right) {
+  let midIdx = Math.floor((left + right) / 2);
+  let currentValue = arr[midIdx];
+
+  console.log(`midIdx ${midIdx} currentValue ${currentValue} `);
+  console.log();
+
+  if (currentValue < target) {
+    left = midIdx + 1;
+  } else if (currentValue > target) {
+    right = midIdx - 1;
+    console.log(`Set end index on index ${midIdx}`);
+  } else {
+    console.log(`You have found the target value ${target} on index ${midIdx}`);
+    found = true;
+    break;
+  }
+
+  if (midIdx === 0 || midIdx === arr.length - 1) {
+    console.log("edge case");
+    break;
+  }
+}
+
+if (!found) {
+  console.log('Number was not found in the array')
+}
